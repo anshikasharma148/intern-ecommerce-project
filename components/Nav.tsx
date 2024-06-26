849fb7d47f918856234825707d28c80516cd1ace
anshikasharma148 <93460776+anshikasharma148@users.noreply.github.com>
2024-06-26 13:52:46 +0530

    feat: responsive collection page

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,5 +1,6 @@
+// components/Widget.tsx
 'use client';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 
 export default function Widget() {
@@ -8,11 +9,7 @@ export default function Widget() {
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
+      setIsScrolled(window.scrollY > 0);
     };
 
     window.addEventListener('scroll', handleScroll);
@@ -30,15 +27,14 @@ export default function Widget() {
             : 'bg-white/30 backdrop-blur-md'
         }`}
       >
-        <div className="p-4 flex justify-between items-center h-full">
-          {/* Logo Container */}
+        <div className="max-w-[1200px] 2xl:max-w-[1440px] mx-auto p-4 flex justify-between items-center h-full">
           <div className="flex items-center">
             <Image
               src={'/logo.png'}
               alt="logo"
               width={150}
               height={300}
-              style={{width: 'auto', height: '100%'}}
+              style={{ width: 'auto', height: '100%' }}
             />
           </div>
 
@@ -65,24 +61,16 @@ export default function Widget() {
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <g
-                id="SVGRepo_bgCarrier"
-                stroke-width="0"
-              ></g>
-              <g
-                id="SVGRepo_tracerCarrier"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              ></g>
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
               <g id="SVGRepo_iconCarrier">
-                {' '}
                 <path
                   d="M16.6725 16.6412L21 21M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z"
                   stroke="#000"
                   stroke-width="1.968"
                   stroke-linecap="round"
                   stroke-linejoin="round"
-                ></path>{' '}
+                ></path>
               </g>
             </svg>
             <div className="bg-orange-500 rounded-full p-2 cursor-pointer">
@@ -94,24 +82,16 @@ export default function Widget() {
                 xmlns="http://www.w3.org/2000/svg"
                 stroke="#fff"
               >
-                <g
-                  id="SVGRepo_bgCarrier"
-                  stroke-width="0"
-                ></g>
-                <g
-                  id="SVGRepo_tracerCarrier"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                ></g>
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
                 <g id="SVGRepo_iconCarrier">
-                  {' '}
                   <path
                     d="M6.29977 5H21L19 12H7.37671M20 16H8L6 3H3M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z"
                     stroke="#fff"
                     stroke-width="1.5"
                     stroke-linecap="round"
                     stroke-linejoin="round"
-                  ></path>{' '}
+                  ></path>
                 </g>
               </svg>
             </div>
@@ -123,21 +103,13 @@ export default function Widget() {
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <g
-                  id="SVGRepo_bgCarrier"
-                  stroke-width="0"
-                ></g>
-                <g
-                  id="SVGRepo_tracerCarrier"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                ></g>
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
                 <g id="SVGRepo_iconCarrier">
-                  {' '}
                   <path
                     d="M8.96173 18.9109L9.42605 18.3219L8.96173 18.9109ZM12 5.50063L11.4596 6.02073C11.601 6.16763 11.7961 6.25063 12 6.25063C12.2039 6.25063 12.399 6.16763 12.5404 6.02073L12 5.50063ZM15.0383 18.9109L15.5026 19.4999L15.0383 18.9109ZM9.42605 18.3219C7.91039 17.1271 6.25307 15.9603 4.93829 14.4798C3.64922 13.0282 2.75 11.3345 2.75 9.1371H1.25C1.25 11.8026 2.2205 13.8221 3.81672 15.4758C5.24723 17.0866 7.07077 18.3752 8.49742 19.4999L9.42605 18.3219ZM2.75 9.1371C2.75 6.98623 3.96537 5.18252 5.62422 4.42419C7.22207 3.68748 9.40166 3.88258 11.4596 6.02073L12.5404 4.98053C10.0985 2.44352 7.26409 2.02539 5.00076 3.05996C2.78471 4.07292 1.25 6.42503 1.25 9.1371H2.75ZM8.49742 19.4999C9.00965 19.9037 9.55954 20.3343 10.1168 20.6599C10.6739 20.9854 11.3096 21.25 12 21.25V19.75C11.6904 19.75 11.3261 19.6293 10.8722 19.2248C10.4213 19.1005 9.95208 18.7226 9.42605 18.3219L8.49742 19.4999ZM15.5026 19.4999C16.9292 18.3752 18.7528 17.0866 20.1833 15.4758C21.6395 13.8221 22.75 11.8026 22.75 9.1371H21.25C21.25 11.3345 20.3508 13.0282 19.0617 14.4798C17.7469 15.9603 16.0896 17.1271 14.574 18.3219L15.5026 19.4999ZM22.75 9.1371C22.75 6.42503 21.2153 4.07292 18.9992 3.05996C16.7359 2.02539 13.9015 2.44352 11.4596 4.98053L12.5404 6.02073C14.5983 3.88258 16.7639 3.68748 18.3756 4.42419C20.0346 5.18252 21.25 6.98623 21.25 9.1371H22.75ZM14.574 18.3219C14.0479 18.7226 13.5787 19.1005 13.1264 19.2248C12.6739 19.6293 12.3096 19.75 12 19.75V21.25C12.6904 21.25 13.3261 20.9854 13.8832 20.6599C14.4405 20.3343 14.9903 19.9037 15.5026 19.4999L14.574 18.3219Z"
                     fill="#fff"
-                  ></path>{' '}
+                  ></path>
                 </g>
               </svg>
             </div>
@@ -149,21 +121,15 @@ export default function Widget() {
                 height="33px"
                 viewBox="0 0 24 24"
               >
-                <g
-                  id="SVGRepo_bgCarrier"
-                  stroke-width="0"
-                ></g>
-                <g
-                  id="SVGRepo_tracerCarrier"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                ></g>
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
                 <g id="SVGRepo_iconCarrier">
                   <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z"></path>
                 </g>
               </svg>
             </div>
           </div>
+
           {/* Mobile Menu Toggle */}
           <div className="md:hidden">
             <button
@@ -188,23 +154,23 @@ export default function Widget() {
             </button>
           </div>
         </div>
-
-        {/* Mobile Menu */}
-        {isMobileMenuOpen && (
-          <div className="md:hidden flex flex-col items-center bg-white/90 backdrop-blur-md p-4 space-y-2">
-            {['Hero', 'Products', 'Shop', 'Pages', 'Sale', 'Contact'].map(
-              (item) => (
-                <div
-                  key={item}
-                  className="text-black font-bold text-[1rem] cursor-pointer hover:text-orange-500 transition-colors duration-200 ease-in font-primary"
-                >
-                  {item}
-                </div>
-              )
-            )}
-          </div>
-        )}
       </nav>
+
+      {/* Mobile Menu */}
+      {isMobileMenuOpen && (
+        <div className="md:hidden flex flex-col items-center bg-white/90 backdrop-blur-md p-4 space-y-2 fixed top-[74px] left-0 w-full z-50">
+          {['Hero', 'Products', 'Shop', 'Pages', 'Sale', 'Contact'].map(
+            (item) => (
+              <div
+                key={item}
+                className="text-black font-bold text-[1rem] cursor-pointer hover:text-orange-500 transition-colors duration-200 ease-in font-primary"
+              >
+                {item}
+              </div>
+            )
+          )}
+        </div>
+      )}
     </div>
   );
 }
